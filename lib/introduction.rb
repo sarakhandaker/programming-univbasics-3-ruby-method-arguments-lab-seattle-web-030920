dc6725c0a90e026a376cfd76b6068eb061f4b5c1
Sara Khandaker <kttsara@hotmail.com>
2020-02-07 18:17:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,10 +9,12 @@ def introduction (name)
 end
 
 def introduction_with_language (name, language)
-  "Hi, my name is #{name}."
+  "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-
+def introduction_with_language_optional (name, language="Ruby")
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
